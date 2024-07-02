4f6b48548860fb92cbf37a49986c7bf81c484960
amberjackman <feverhong@vivaldi.net>
2024-07-02 18:54:25 +0900

    메타태그 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,13 @@ import "../public/globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "LoL In-Game Analyzer",
-  description: "Analyze current League of Legends games",
+  title: "Am I Noisy?",
+  description: "난 얼마나 시끄러울까?",
+  ogTitle: "Am I Noisy?",
+  ogDescription: "난 얼마나 시끄러울까? - 나의 핑 횟수를 확인하세요.",
+  ogType: "website",
+  twitterTitle: "Am I Noisy?",
+  twitterDescription: "난 얼마나 시끄러울까? - 나의 핑 횟수를 확인하세요",
 };
 
 export default function RootLayout({
