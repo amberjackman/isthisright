67c2e3f6d807f16b0fc28116bef096cedbddb047
amberjackman <feverhong@vivaldi.net>
2024-07-04 18:44:55 +0900

    디자인 수정

--- app/page.tsx
+++ app/page.tsx
@@ -54,11 +54,21 @@ const MyComponent = () => {
         <div className="w-9/12 max-w-lg px-4">
           <div className="flex flex-col items-center">
             <img
-              className="w-40 h-40 mb-8"
+              className="w-40 h-40 mb-3"
               src="/images/Landing_Logo_design.png"
               alt="logo"
             />
-            <div className=" w-full p-6 rounded-lg shadow-lg text-white">
+
+            <div className="flex w-lvw h-5 m-3 flex-nowrap overflow-hidden transition duration-300 shadow-xl text-md font-bold bg-gray-600 opacity-95 text-gray-400 hover:text-white">
+              <div className="flex flow-wrap h-6 w-fit animate-textLoop hover:pause-textLoop pr-[1.5vw]">
+                <div className="whitespace-nowrap">
+                  hide on bush#KR1(FAKER) : 27.4회 T1 Gumayusi#KR1 : 74회
+                  kiin#KR1 : 11.2회 JUGKING#KR1(Canyon) : 8.6회 God
+                  Thunder#KR1(Zeus) : 86회{" "}
+                </div>
+              </div>
+            </div>
+            <div className=" w-full p-6 rounded-lg shadow-2xl bg-gray-200">
               <form onSubmit={handleFormSubmit} className="space-y-4">
                 <input
                   type="text"
@@ -86,38 +96,64 @@ const MyComponent = () => {
               {matches.length > 0 && (
                 <div className="mt-4">
                   <ul className="space-y-2">
-                    {matches.map((match) => (
-                      <li
-                        key={match.matchId}
-                        className="bg-gray-800 p-4 rounded-lg "
-                      >
-                        {/* <p className="text-white">Match ID: {match.matchId}</p> */}
-                        {/* <h3 className="font-semibold mt-2 text-white">
-                          Ping Data:
-                        </h3> */}
-                        {Object.entries(match.pingData).map(([key, value]) => (
-                          <div
-                            key={key}
-                            className="flex flex-row items-center justify-center gap-5 w-full"
-                          >
-                            {pingImages[key] && (
-                              <img
-                                src={pingImages[key]}
-                                alt={key}
-                                className="w-14 h-13"
-                              />
-                            )}
-                            <p
-                              className={`text-white ${
-                                value > 10 ? "text-red-500" : "text-blue-100"
-                              }`}
-                            >
-                              {value}
-                            </p>
-                          </div>
-                        ))}
-                      </li>
-                    ))}
+                    {matches.length > 0 && (
+                      <div className="mt-4">
+                        {(() => {
+                          const totalSum = matches.reduce((acc, match) => {
+                            return (
+                              acc +
+                              Object.values(match.pingData).reduce(
+                                (sum, value) => sum + value,
+                                0
+                              )
+                            );
+                          }, 0);
+                          const overallAverage = totalSum / 5;
+
+                          return (
+                            <>
+                              <div className="mb-4 text-white text-center bg-gray-700 p-3 rounded-lg">
+                                경기당 평균 핑 횟수: {overallAverage.toFixed(2)}
+                              </div>
+                              <ul className="space-y-2">
+                                {matches.map((match) => (
+                                  <li
+                                    key={match.matchId}
+                                    className="bg-gray-800 p-4 rounded-lg"
+                                  >
+                                    {Object.entries(match.pingData).map(
+                                      ([key, value]) => (
+                                        <div
+                                          key={key}
+                                          className="flex flex-row items-center justify-center gap-5 w-full"
+                                        >
+                                          {pingImages[key] && (
+                                            <img
+                                              src={pingImages[key]}
+                                              alt={key}
+                                              className="w-14 h-13"
+                                            />
+                                          )}
+                                          <p
+                                            className={`text-white ${
+                                              value > 10
+                                                ? "text-red-500"
+                                                : "text-blue-100"
+                                            }`}
+                                          >
+                                            {value}
+                                          </p>
+                                        </div>
+                                      )
+                                    )}
+                                  </li>
+                                ))}
+                              </ul>
+                            </>
+                          );
+                        })()}
+                      </div>
+                    )}
                   </ul>
                 </div>
               )}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,29 @@
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    extend: {
+      keyframes: {
+        textLoop: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+      },
+      animation: {
+        textLoop: "textLoop 25s linear infinite",
+      },
+      colors: {
+        customGray: "#9D9993",
+      },
+    },
     fontFamily: {
       sans: ["var(--nanum_gothic)"],
       happySans: ["var(--press_start_2p)"],
     },
   },
   plugins: [],
+  variants: {
+    extend: {
+      animation: ["hover"],
+    },
+  },
 };
